ee4a5471fb26577da640cf124b436e26f1ce3702
fago <nuppla@zites.net>
2011-05-25 13:08:02 +0200

    overhauled profile2_access() and introduced hook_profile2_access() so modules may alter access to profiles.

--- profile2.api.php
+++ profile2.api.php
@@ -272,5 +272,42 @@ function hook_form_profile2_form_alter(&$form, &$form_state) {
 }
 
 /**
+ * Control access to profiles.
+ *
+ * Modules may implement this hook if they want to have a say in whether or not
+ * a given user has access to perform a given operation on a profile.
+ *
+ * @param $op
+ *   The operation being performed. One of 'view', 'edit' (being the same as
+ *   'create' or 'update') and 'delete'.
+ * @param $profile
+ *   (optional) A profile to check access for. If nothing is given, access for
+ *   all profiles is determined.
+ * @param $account
+ *   (optional) The user to check for. If no account is passed, access is
+ *   determined for the global user.
+ * @return boolean
+ *   Return TRUE to grant access, FALSE to explicitly deny access. Return NULL
+ *   or nothing to not affect the operation.
+ *   Access is granted as soon as a module grants access and no one denies
+ *   access. Thus if no module explicitly grants access, access will be denied.
+ *
+ * @see profile2_access()
+ */
+function hook_profile2_access($op, $profile = NULL, $account = NULL) {
+  if (isset($profile)) {
+    // Explicitly deny access for a 'secret' profile type.
+    if ($profile->type == 'secret' && !user_access('custom permission')) {
+      return FALSE;
+    }
+    // For profiles other than the default profile grant access.
+    if ($profile->type != 'main' && user_access('custom permission')) {
+      return TRUE;
+    }
+    // In other cases do not alter access.
+  }
+}
+
+/**
  * @}
  */
