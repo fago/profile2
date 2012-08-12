623967eb675d39d8724762a94be7cd74a70a8ae6
sun <sun@unleashedmind.com>
2012-08-12 23:23:17 +0200

    - #1726822 by n3or, sun: Updated Profile and ProfileType classes to D8.

---
+++ lib/Drupal/profile2/Profile.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\profile2\Profile.
+ */
+
+namespace Drupal\profile2;
+
+use Drupal\entity\Entity;
+
+/**
+ * The class used for profile entities.
+ */
+class Profile extends Entity {
+
+  /**
+   * The profile id.
+   *
+   * @var integer
+   */
+  public $pid;
+
+  /**
+   * The name of the profile type.
+   *
+   * @var string
+   */
+  public $type;
+
+  /**
+   * The profile label.
+   *
+   * @var string
+   */
+  public $label;
+
+  /**
+   * The user id of the profile owner.
+   *
+   * @var integer
+   */
+  public $uid;
+
+  /**
+   * The Unix timestamp when the profile was created.
+   *
+   * @var integer
+   */
+  public $created;
+
+  /**
+   * The Unix timestamp when the profile was most recently saved.
+   *
+   * @var integer
+   */
+  public $changed;
+
+
+  public function __construct($values = array()) {
+    if (isset($values['user'])) {
+      $this->setUser($values['user']);
+      unset($values['user']);
+    }
+    if (isset($values['type']) && is_object($values['type'])) {
+      $values['type'] = $values['type']->type;
+    }
+    if (!isset($values['label']) && isset($values['type']) && $type = profile2_get_types($values['type'])) {
+      // Initialize the label with the type label, so newly created profiles
+      // have that as interim label.
+      $values['label'] = $type->label;
+    }
+    parent::__construct($values, 'profile2');
+  }
+
+  /**
+   * Returns the user owning this profile.
+   */
+  public function user() {
+    return user_load($this->uid);
+  }
+
+  /**
+   * Sets a new user owning this profile.
+   *
+   * @param $account
+   *   The user account object or the user account id (uid).
+   */
+  public function setUser($account) {
+    $this->uid = is_object($account) ? $account->uid : $account;
+  }
+
+  /**
+   * Gets the associated profile type object.
+   *
+   * @return ProfileType
+   */
+  public function type() {
+    return profile2_get_types($this->type);
+  }
+
+  /**
+   * Returns the full url() for the profile.
+   */
+  public function url() {
+    $uri = $this->uri();
+    return url($uri['path'], $uri);
+  }
+
+  /**
+   * Returns the drupal path to this profile.
+   */
+  public function path() {
+    $uri = $this->uri();
+    return $uri['path'];
+  }
+
+  public function defaultUri() {
+    return array(
+      'path' => 'user/' . $this->uid,
+      'options' => array('fragment' => 'profile-' . $this->type),
+    );
+  }
+
+  public function defaultLabel() {
+    if (module_exists('profile2_i18n')) {
+      // Run the label through i18n_string() using the profile2_type label
+      // context, so the default label (= the type's label) gets translated.
+      return entity_i18n_string('profile2:profile2_type:' . $this->type . ':label', $this->label);
+    }
+    return $this->label;
+  }
+
+  public function buildContent($view_mode = 'full', $langcode = NULL) {
+    $content = array();
+    // Assume newly create objects are still empty.
+    if (!empty($this->is_new)) {
+      $content['empty']['#markup'] = '<em class="profile2-no-data">' . t('There is no profile data yet.') . '</em>';
+    }
+    return entity_get_controller($this->entityType)->buildContent($this, $view_mode, $langcode, $content);
+  }
+
+  public function save() {
+    // Care about setting created and changed values. But do not automatically
+    // set a created values for already existing profiles.
+    if (empty($this->created) && (!empty($this->is_new) || !$this->pid)) {
+      $this->created = REQUEST_TIME;
+    }
+    $this->changed = REQUEST_TIME;
+
+    parent::save();
+    // Update the static cache from profile2_load_by_user().
+    $cache = &drupal_static('profile2_load_by_user', array());
+    if (isset($cache[$this->uid])) {
+      $cache[$this->uid][$this->type] = $this->pid;
+    }
+  }
+}
+
---
+++ lib/Drupal/profile2/ProfileType.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\profile2\ProfileType.
+ */
+
+namespace Drupal\profile2;
+
+use Drupal\entity\Entity;
+
+/**
+ * Use a separate class for profile types so we can specify some defaults
+ * modules may alter.
+ */
+class ProfileType extends Entity {
+
+  /**
+   * Whether the profile type appears in the user categories.
+   */
+  public $userCategory = TRUE;
+
+  /**
+   * Whether the profile is displayed on the user account page.
+   */
+  public $userView = TRUE;
+
+  public $type;
+  public $label;
+  public $weight = 0;
+
+  public function __construct($values = array()) {
+    parent::__construct($values, 'profile2_type');
+  }
+
+  /**
+   * Returns whether the profile type is locked, thus may not be deleted or renamed.
+   *
+   * Profile types provided in code are automatically treated as locked, as well
+   * as any fixed profile type.
+   */
+  public function isLocked() {
+    return isset($this->status) && empty($this->is_new) && (($this->status & ENTITY_IN_CODE) || ($this->status & ENTITY_FIXED));
+  }
+
+  /**
+   * Overridden, to introduce the method for old entity API versions (BC).
+   *
+   * @todo Remove once we bump the required entity API version.
+   */
+  public function getTranslation($property, $langcode = NULL) {
+    if (module_exists('profile2_i18n')) {
+      return parent::getTranslation($property, $langcode);
+    }
+    return $this->$property;
+  }
+}
