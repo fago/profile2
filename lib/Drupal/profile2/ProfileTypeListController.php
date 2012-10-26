1c236a9c8997b382d7f7f5367fb9debde52e25fe
n3or <n3or@1297560.no-reply.drupal.org>
2012-10-26 16:57:52 +0200

    - #1726822 by n3or, sun: Updated for EntityListController.

---
+++ lib/Drupal/profile2/ProfileTypeListController.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\profile2\ProfileTypeListController.
+ */
+
+namespace Drupal\profile2;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListController;
+
+/**
+ * List controller for profile types.
+ */
+class ProfileTypeListController extends EntityListController {
+
+  /**
+   * Overrides Drupal\Core\Entity\EntityListController::getOperations().
+   */
+  public function getOperations(EntityInterface $entity) {
+    $operations = parent::getOperations($entity);
+    if (module_exists('field_ui')) {
+      $uri = $entity->uri();
+      $operations['manage-fields'] = array(
+        'title' => t('Manage fields'),
+        'href' => $uri['path'] . '/fields',
+        'options' => $uri['options'],
+        'weight' => 11,
+      );
+      $operations['manage-display'] = array(
+        'title' => t('Manage display'),
+        'href' => $uri['path'] . '/display',
+        'options' => $uri['options'],
+        'weight' => 12,
+      );
+    }
+    return $operations;
+  }
+
+}
